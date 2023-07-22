1368765d4dfe0e6bfe69610c83d9a496567f04f8
estanip <estani.pettigrew@gmail.com>
2023-07-22 17:29:19 -0300

    Add DB connection

---
+++ src/db/connection.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { env } from "../../env";
+
+export const connection = async () => {
+  try {
+    await mongoose.connect(env.MONGO_URI);
+    console.log("DB Succesfully Conected");
+  } catch (error) {
+    console.log(error);
+  }
+};
