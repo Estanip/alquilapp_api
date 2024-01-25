a205bd1345b8316be4fea57c39dd552267e2fcf5
estanip <estani.pettigrew@gmail.com>
2024-01-25 15:22:40 -0300

    Add validations to Pricing

--- src/modules/pricing/schemas/pricing.schema.ts
+++ src/modules/pricing/schemas/pricing.schema.ts
@@ -3,9 +3,11 @@ import { IPricing, IPricingDocument } from '../interfaces/pricing.interfaces';
 import { NextFunction } from 'express';
 import { CurrencyTypes } from '../entities/pricing.entity';
 import { PricingModel } from '../models/pricing.model';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 import { MembershipTypes } from 'src/modules/membership_type/entities/membership_type.entity';
 import { CourtNumbers } from 'src/modules/court/entities/court.entity';
+import { CourtModel } from 'src/modules/court/models/court.model';
+import { ICourtDocument } from 'src/modules/court/interfaces/court.interfaces';
 
 export const PricingSchema: Schema = new Schema<IPricing>(
     {
@@ -44,6 +46,23 @@ PricingSchema.pre('validate', function (next: NextFunction) {
         .then((data: IPricingDocument) => {
             if (!data) return next();
             else return next(new ConflictException('The pricing just exists'));
+        })
+        .catch((err: unknown) => console.log(err));
+});
+
+/* DATE VALIDATION */
+PricingSchema.pre('validate', function (next: NextFunction) {
+    const validateUntilDate = new Date(`${this.validate_until.toISOString().substring(0, 10)}`);
+    const currentDate = new Date();
+    if (validateUntilDate < currentDate) return next(new ConflictException('Date error'));
+    else return next();
+});
+
+PricingSchema.pre('validate', function (next: NextFunction) {
+    CourtModel.findOne({ court_number: this.court })
+        .then((data: ICourtDocument) => {
+            if (data) return next();
+            else return next(new NotFoundException('Court does not exists'));
         })
         .catch((err: unknown) => console.log(err));
 });
