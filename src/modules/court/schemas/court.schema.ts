43b9d2c3b63b8b66a6fa4eed4afa0e604de68449
estanip <estani.pettigrew@gmail.com>
2024-03-02 14:33:14 -0300

    refactor: improve types

--- src/modules/court/schemas/court.schema.ts
+++ src/modules/court/schemas/court.schema.ts
@@ -1,12 +1,13 @@
 import { SchemaFactory } from '@nestjs/mongoose';
 import { NextFunction } from 'express';
+import { IMongooseError } from 'src/shared/interfaces';
 import { CourtSchema } from './CourtSchema';
 
 export const courtSchema = SchemaFactory.createForClass(CourtSchema);
 
 // Validate court number be unique
-courtSchema.post('save', function (error: any, doc: any, next: NextFunction): void {
+courtSchema.post('save', function (error: IMongooseError, doc: Document, next: NextFunction): void {
     if (error.name === 'MongoServerError' && error.code === 11000)
-        return next(new Error('Court number must be unique'));
+        return next(new Error('Court must be unique'));
     else return next(error);
 });
