360301a213565765d3aac8c1833ea6ace1943812
estanip <estani.pettigrew@gmail.com>
2024-03-02 14:32:39 -0300

    refactor: improve auth interface

--- src/modules/auth/interfaces/auth.interfaces.ts
+++ src/modules/auth/interfaces/auth.interfaces.ts
@@ -1,14 +1,3 @@
-import { Types } from 'mongoose';
-
-export interface ILoginResponse {
-    id: Types.ObjectId;
-    email: string;
-    first_name: string;
-    last_name: string;
-    identification_number: string;
-    token: string;
-}
-
 export interface IJWTPayload {
     _id: string;
 }
