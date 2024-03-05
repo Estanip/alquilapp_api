c67d30579786765ebaf546238fcb18e7150ae8ed
estanip <estani.pettigrew@gmail.com>
2024-03-05 11:35:53 -0300

    refactor: improve types

--- src/modules/reservation/reservation.service.ts
+++ src/modules/reservation/reservation.service.ts
@@ -2,11 +2,14 @@ import { ConflictException, HttpStatus, Injectable, NotFoundException } from '@n
 import { SHIFT_DURATION } from 'src/constants/reservations.constants';
 import { SuccessResponse } from 'src/shared/responses/SuccessResponse';
 import { CourtRepository } from '../court/court.repository';
-import { CourtSchema } from '../court/schemas/CourtSchema';
+import { ICourtDocument } from '../court/interfaces/court.interfaces';
+import { IPricingDocument } from '../pricing/interfaces/pricing.interfaces';
 import { PricingRepository } from '../pricing/pricing.repository';
-import { PricingSchema } from '../pricing/schemas/PricingSchema';
-import { IUserAttributes } from '../users/interfaces/user.interface';
-import { UserSchema } from '../users/schemas/UserSchema';
+import {
+    IUserAttributes,
+    IUserDocument,
+    TUserCollection,
+} from '../users/interfaces/user.interface';
 import { UserRepository } from '../users/user.repository';
 import { CreateReservationDtoRequest } from './dto/request/create-reservation.dto';
 import { UpdateReservationDtoRequest } from './dto/request/update-reservation.dto';
@@ -14,10 +17,8 @@ import { AvailavilitiesResponseDto } from './dto/response/availability.dto';
 import { ByOwnerResponseDto } from './dto/response/by_owner.dto';
 import { ReservationsResponseDto } from './dto/response/index.dto';
 import { IPlayer } from './interfaces/player.interfaces';
-import { TReservationCollection } from './interfaces/reservation.interfaces';
+import { IReservationDocument, TReservationCollection } from './interfaces/reservation.interfaces';
 import { ReservationRepository } from './reservation.repository';
-import { PlayerSchema } from './schemas/PlayerSchema';
-import { ReservationSchema } from './schemas/ReservationSchema';
 
 @Injectable()
 export class ReservationService {
@@ -35,7 +36,7 @@ export class ReservationService {
     }
 
     async getAll() {
-        const data: ReservationSchema[] = await this.reservationRepository.findAllWithPopulate(
+        const data: TReservationCollection = await this.reservationRepository.findAllWithPopulate(
             'players.user',
             'first_name last_name membership_type',
         );
@@ -47,10 +48,10 @@ export class ReservationService {
     }
 
     async getByDateAndCourt(court: number, date: string) {
-        const data: TReservationCollection = await this.reservationRepository.findAll({
+        const data = (await this.reservationRepository.findAll({
             court,
             date: new Date(date)?.toISOString()?.substring(0, 10),
-        });
+        })) as TReservationCollection;
         return new SuccessResponse(
             HttpStatus.OK,
             'Reservations found',
@@ -59,9 +60,9 @@ export class ReservationService {
     }
 
     async getByOwnerId(id: string) {
-        const data: TReservationCollection = await this.reservationRepository.findAll({
+        const data = (await this.reservationRepository.findAll({
             owner_id: id,
-        });
+        })) as TReservationCollection;
         return new SuccessResponse(
             HttpStatus.OK,
             'Reservations found',
@@ -70,7 +71,7 @@ export class ReservationService {
     }
 
     async getById(id: string) {
-        const data: ReservationSchema = await this.reservationRepository.findById(id, true);
+        const data = (await this.reservationRepository.findById(id, true)) as IReservationDocument;
         return new SuccessResponse(
             HttpStatus.OK,
             'Reservation found',
@@ -92,19 +93,19 @@ export class ReservationService {
     async _setPrice(data: CreateReservationDtoRequest | UpdateReservationDtoRequest) {
         for (const player of data.players) {
             const user_membership = (
-                (await this.userRepository.findById(player.user.toString(), true)) as UserSchema
+                (await this.userRepository.findById(player.user.toString(), true)) as IUserDocument
             )?.membership_type;
             const price = (
                 (await this.pricingRepository.findOne({
                     court: data.court,
                     membership_type: user_membership,
-                })) as PricingSchema
+                })) as IPricingDocument
             )?.price;
             if (price === null || price === undefined)
                 throw new NotFoundException('Error when try to get price associated');
             else player.fee = price;
         }
-        const total_price = data.players.reduce((acc: number, cur: PlayerSchema) => {
+        const total_price = data.players.reduce((acc: number, cur: IPlayer) => {
             return acc + cur.fee;
         }, 0);
 
@@ -132,14 +133,14 @@ export class ReservationService {
     }
 
     async _validateAvailability(data: CreateReservationDtoRequest | UpdateReservationDtoRequest) {
-        const reservation: ReservationSchema = await this.reservationRepository.findOne(
+        const reservation = (await this.reservationRepository.findOne(
             {
                 date: data.date,
                 from: data.from,
                 court: data.court,
             },
             false,
-        );
+        )) as IReservationDocument;
         if (reservation) throw new ConflictException('The court shift is taken');
     }
 
@@ -148,7 +149,7 @@ export class ReservationService {
         if (playersIds.some((id: string, index: number) => playersIds.indexOf(id) != index))
             throw new ConflictException('Repeated players');
 
-        const users: UserSchema[] = [];
+        const users: TUserCollection = [];
         for (const player of players) {
             users.push(await this.userRepository.findById(player.user.toString(), false));
         }
@@ -159,7 +160,7 @@ export class ReservationService {
         if (users.some((user: IUserAttributes) => !user.is_membership_validated))
             throw new NotFoundException('User membership has not yet been validated');
 
-        const reservations: ReservationSchema[] = await this.reservationRepository.aggregate([
+        const reservations = (await this.reservationRepository.aggregate([
             {
                 $project: {
                     players: 1,
@@ -178,7 +179,7 @@ export class ReservationService {
                     'players.user': { $in: playersIds },
                 },
             },
-        ]);
+        ])) as TReservationCollection;
 
         if (reservations.length) {
             reservations.map((reservation) => {
@@ -192,7 +193,10 @@ export class ReservationService {
     }
 
     async _validateCourt(court_number: number, from: string) {
-        const court: CourtSchema = await this.courtRepository.findOne({ court_number }, true);
+        const court = (await this.courtRepository.findOne(
+            { court_number },
+            true,
+        )) as ICourtDocument;
         if (court)
             if (from.substring(0, 2) < court.available_from.substring(0, 2))
                 throw new ConflictException('From is too early');
