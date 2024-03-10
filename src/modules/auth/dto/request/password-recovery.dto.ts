f677f767f5281b6051ef52f10b24c0fe3c711460
estanip <estani.pettigrew@gmail.com>
2024-03-10 16:54:13 -0300

    feat: add password reset dto

---
+++ src/modules/auth/dto/request/password-recovery.dto.ts
@@ -0,0 +1,6 @@
+import { PickType } from '@nestjs/swagger';
+import { LoginDto } from './login-auth.dto';
+
+export class ChangePasswordDto extends PickType(LoginDto, ['email']) {
+    new_password: string;
+}
