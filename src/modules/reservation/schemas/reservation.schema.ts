9944671afdc444e381adf9896bc655001560ca05
estanip <estani.pettigrew@gmail.com>
2024-03-11 10:34:42 -0300

    refactor: improve validation pre create

--- src/modules/reservation/schemas/reservation.schema.ts
+++ src/modules/reservation/schemas/reservation.schema.ts
@@ -11,22 +11,10 @@ reservationSchema.pre('validate', function (next: NextFunction) {
     if (this.to)
         if (Number(this.from.substring(0, 2)) > Number(this.to.substring(0, 2)))
             throw new ConflictException('Scheduling error');
-    const fromDate = new Date(`${this.date.toISOString().substring(0, 10)}T${this.from}:00`);
-    // REVIEW
-    const toDate = new Date(
-        `${this.date.toISOString().substring(0, 10)}T${
-            Number(this.from.substring(0, 2)) + SHIFT_DURATION.ONE_HOUR
-        }:00`,
-    );
-    const currentDate = new Date();
-    if (toDate < currentDate) return next(new ConflictException('Date error'));
-    if (!_isTimeDifferenceOneHour(fromDate, toDate))
+    if (!_isTimeDifferenceOneHour(this.from, this.to))
         return next(new ConflictException('The shift must be one hour'));
     else return next();
 });
 
-const _isTimeDifferenceOneHour = (from: Date, to: Date) => {
-    const differenceInMilliseconds = Math.abs(to.getTime() - from.getTime());
-    const oneHourInMilliseconds = 60 * 60 * 1000;
-    return differenceInMilliseconds === oneHourInMilliseconds;
-};
+const _isTimeDifferenceOneHour = (from: string, to: string) =>
+    parseInt(to.substring(0, 2)) - parseInt(from.substring(0, 2)) === SHIFT_DURATION.ONE_HOUR;
