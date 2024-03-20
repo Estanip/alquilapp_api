62c38c8985073a946340758daf3bd8170d589d17
estanip <estani.pettigrew@gmail.com>
2024-03-20 14:16:02 -0300

    feat: add user verification code EPs

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { ApiBody, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { IsPublic } from 'src/shared/middlewares/public_routes.config';
 import { SuccessResponse } from 'src/shared/responses/SuccessResponse';
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/request/login-auth.dto';
 import { ChangePasswordDto } from './dto/request/password-recovery.dto';
 import { RegisterDto } from './dto/request/register-auth.dto';
+import { ResendVerificationCodeDto } from './dto/request/user_verification_code.dto';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -41,5 +42,18 @@ export class AuthController {
     @Put('/change-password')
     resetPassword(@Body() passwordReset: ChangePasswordDto) {
         return this.authService.changePassword(passwordReset);
+    }
+
+    @Get('/code-verification/:user_id/:code')
+    checkCodeVerifiation(@Param('user_id') user_id: string, @Param('code') code: string) {
+        return this.authService.checkCodeVerifiation(user_id, code);
+    }
+
+    @Post('/resend-verification-code/:user_id')
+    resendVerificationCode(
+        @Param('user_id') user_id: string,
+        @Body() data: ResendVerificationCodeDto,
+    ) {
+        return this.authService.resendVerificationCode(user_id, data);
     }
 }
