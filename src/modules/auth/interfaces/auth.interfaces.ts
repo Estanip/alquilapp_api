fbdd5a58d9376df2946935e1e8607658fedb0203
estanip <estani.pettigrew@gmail.com>
2024-03-20 14:18:35 -0300

    feat: add user verification code interfaces

--- src/modules/auth/interfaces/auth.interfaces.ts
+++ src/modules/auth/interfaces/auth.interfaces.ts
@@ -1,3 +1,12 @@
+import { Document } from 'mongoose';
+
 export interface IJWTPayload {
     _id: string;
 }
+
+export interface IUserVerificationCode {
+    user: string;
+    code: string;
+}
+
+export interface IUserCodeVerificationDocument extends IUserVerificationCode, Document {}
