fce580e4d0af2d792e0f7484fa6c517ec8344dd8
estanip <estani.pettigrew@gmail.com>
2024-04-01 10:40:07 -0300

    refactor: improve players interface & fix imports

--- src/modules/reservation/dto/response/by_owner.dto.ts
+++ src/modules/reservation/dto/response/by_owner.dto.ts
@@ -1,20 +1,39 @@
 import { TimeZones } from 'src/constants';
-import {
-    IReservation,
-    IReservationDocument,
-    TReservationCollection,
-} from '../../interfaces/reservation.interfaces';
+import { IPlayer } from 'src/modules/users/modules/player/interfaces';
+import { IPlayerPopulate, IReservation, IReservationDocument } from '../../interfaces';
+import { IReservationSchemaWithPlayerPopulate, ReservationSchema } from '../../schemas';
 
-interface IByOwner extends IReservation {
+type TReservationData = Omit<ReservationSchema, 'players'> & {
+    players: IPlayerPopulate[];
+};
+
+type TPlayerPopulateResponse = {
+    first_name: string;
+    last_name: string;
+    membership_type: string;
+};
+
+type TPlayerResponse = {
+    user_id: string;
+    fee: number;
+};
+
+interface IByOwnerResponse extends Omit<IReservation, 'players'> {
     readonly _id: string;
+    players: TPlayerPopulateResponse[];
+}
+
+interface IByOwnerAndDateResponse extends Omit<IReservation, 'players' | 'total_price'> {
+    readonly _id: string;
+    players: TPlayerResponse[];
 }
 
 type TResponse = {
-    active: IByOwner[];
-    inactive: IByOwner[];
+    active: IByOwnerResponse[];
+    inactive: IByOwnerResponse[];
 };
 
-const _isReservationExpired = (reservation: IReservation) => {
+const _isReservationExpired = (reservation: TReservationData) => {
     const currentDate = new Date().toLocaleString('en-US', { timeZone: TimeZones.ARG });
     const reservationDate = new Date(
         `${reservation.date.substring(0, 10)}T${reservation.from}:00`,
@@ -24,10 +43,10 @@ const _isReservationExpired = (reservation: IReservation) => {
 };
 
 export class ByOwnerResponseDto {
-    static toResponse(data: TReservationCollection): TResponse {
+    static toResponse(data: IReservationSchemaWithPlayerPopulate[]): TResponse {
         let reservations: TResponse = { inactive: [], active: [] };
         if (data?.length > 0) {
-            data?.map((reservation: IReservationDocument) => {
+            data?.map((reservation: TReservationData) => {
                 if (_isReservationExpired(reservation))
                     reservations.inactive.push({
                         _id: reservation._id.toString(),
@@ -35,7 +54,13 @@ export class ByOwnerResponseDto {
                         from: reservation?.from,
                         to: reservation?.to,
                         court: reservation?.court,
-                        players: reservation.players,
+                        players: reservation?.players.map((player: IPlayerPopulate) => {
+                            return {
+                                first_name: player.user_id.first_name,
+                                last_name: player.user_id.last_name,
+                                membership_type: player.user_id.membership_type,
+                            };
+                        }),
                         owner_id: reservation.owner_id,
                         total_price: reservation.total_price,
                     });
@@ -45,7 +70,13 @@ export class ByOwnerResponseDto {
                         date: reservation?.date,
                         from: reservation?.from,
                         to: reservation?.to,
-                        players: reservation.players,
+                        players: reservation?.players.map((player: IPlayerPopulate) => {
+                            return {
+                                first_name: player.user_id.first_name,
+                                last_name: player.user_id.last_name,
+                                membership_type: player.user_id.membership_type,
+                            };
+                        }),
                         owner_id: reservation.owner_id,
                         total_price: reservation.total_price,
                         court: reservation?.court,
@@ -53,5 +84,28 @@ export class ByOwnerResponseDto {
             });
         } else reservations = null;
         return reservations;
+    }
+}
+
+export class ByOwnerAndDateResponseDto {
+    static toResponse(data: IReservationDocument): IByOwnerAndDateResponse {
+        const reservation: IByOwnerAndDateResponse = data
+            ? {
+                  _id: data._id.toString(),
+                  date: data?.date,
+                  from: data?.from,
+                  to: data?.to,
+                  court: data?.court,
+                  players: data?.players.map((player: IPlayer) => {
+                      return {
+                          user_id: player.user_id.toString(),
+                          fee: player.fee,
+                      };
+                  }),
+                  owner_id: data.owner_id,
+              }
+            : null;
+
+        return reservation;
     }
 }
--- src/modules/reservation/dto/response/index.dto.ts
+++ src/modules/reservation/dto/response/index.dto.ts
@@ -1,44 +1,64 @@
-import {
-    IReservationDocument,
-    TReservationCollection,
-} from '../../interfaces/reservation.interfaces';
-import { ReservationSchema } from '../../schemas/ReservationSchema';
+import { IPlayerPopulate } from '../../interfaces';
+import { ReservationSchema } from '../../schemas';
 
-interface IReservationResponse extends Omit<ReservationSchema, '_id'> {
+type TReservationData = Omit<ReservationSchema, 'players'> & {
+    players: IPlayerPopulate[];
+};
+
+type TPlayerPopulateResponse = {
+    first_name: string;
+    last_name: string;
+    membership_type: string;
+};
+interface IReservationResponse extends Omit<ReservationSchema, '_id' | 'owner_id' | 'players'> {
     _id: string;
+    owner_id: string;
+    players: TPlayerPopulateResponse[];
 }
 
-type TResponse = ReservationSchema[];
+type TGetAllResponse = IReservationResponse[];
 export class ReservationsResponseDto {
-    static getAll(data: TReservationCollection): TResponse {
-        const reservations: TResponse = [];
+    static getAll(data: TReservationData[]): TGetAllResponse {
+        const reservations: TGetAllResponse = [];
         if (data?.length > 0) {
-            data?.map((reservation: IReservationDocument) =>
+            data?.map((reservation: TReservationData) =>
                 reservations.push({
                     _id: reservation?._id?.toString(),
-                    owner_id: reservation.owner_id,
+                    owner_id: reservation.owner_id.toString(),
                     date: reservation?.date,
                     from: reservation?.from,
                     court: reservation?.court,
                     total_price: reservation?.total_price,
-                    players: reservation?.players,
+                    players: reservation?.players.map((player: IPlayerPopulate) => {
+                        return {
+                            first_name: player.user_id.first_name,
+                            last_name: player.user_id.last_name,
+                            membership_type: player.user_id.membership_type,
+                        };
+                    }),
                 }),
             );
         }
         return reservations;
     }
 
-    static getOne(data: IReservationDocument): IReservationResponse | null {
+    static getOne(data: TReservationData): IReservationResponse | null {
         let reservation = null;
         if (Object.values(data).length) {
             reservation = {
                 _id: data?._id?.toString(),
-                owner_id: data.owner_id,
+                owner_id: data.owner_id.toString(),
                 date: data?.date,
                 from: data?.from,
                 court: data?.court,
                 total_price: data?.total_price,
-                players: data?.players,
+                players: data?.players.map((player: IPlayerPopulate) => {
+                    return {
+                        first_name: player.user_id.first_name,
+                        last_name: player.user_id.last_name,
+                        membership_type: player.user_id.membership_type,
+                    };
+                }),
             };
         }
         return reservation;
