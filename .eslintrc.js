ece15591ae33cb28b1918bcfcab24e24695afcd8
estanip <estani.pettigrew@gmail.com>
2024-04-15 10:55:43 -0300

    feet: improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
     root: true,
-    env: {
-        es2021: true,
-    },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
-    parserOptions: { project: ['./tsconfig.json'] },
-    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
     rules: {
-        'no-undef': 'off',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off', // Disable explicit return types for public methods in classes
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Allow unused variables starting with underscore
+        //'@typescript-eslint/no-explicit-any': 'off', // Allow the use of 'any' type
+        '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }], // Disallow empty functions, except arrow functions
+        '@typescript-eslint/no-non-null-assertion': 'off', // Allow non-null assertion operator '!'
+        'prettier/prettier': 'error',
         'object-shorthand': ['error', 'always', { avoidQuotes: true }],
         'prefer-destructuring': [
             'error',
