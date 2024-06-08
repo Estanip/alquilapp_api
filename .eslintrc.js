a99eeb0387d401246d5f2ec89f1f62784029cc4b
estanip <estani.pettigrew@gmail.com>
2024-06-08 15:26:50 -0300

    fix: ts config parsing error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
