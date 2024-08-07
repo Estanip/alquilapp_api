0ab7296d25a4457fcd6bdde386b59f226506c959
estanip <estani.pettigrew@gmail.com>
2024-08-07 19:24:05 -0300

    feat: improve morgan middleware

--- src/shared/middlewares/morgan.service.ts
+++ src/shared/middlewares/morgan.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import morgan, { StreamOptions } from 'morgan';
 import { LoggerService } from '../utils/logger/logger.service';
 
@@ -7,17 +7,26 @@ import { LoggerService } from '../utils/logger/logger.service';
 export class MorganMiddleware implements NestMiddleware {
   constructor(private _logger: LoggerService) {}
 
-  use(req: Request, res: Response, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
+    // Get & set service name from url
     let serviceName = req.baseUrl ? req.baseUrl.replace('/', '').toUpperCase() : '';
     serviceName = serviceName.split('').includes('/') ? serviceName.split('/')[0] : serviceName;
+
+    // Create a logger from service name
     this._logger = new LoggerService(`HTTP-${serviceName}`);
+
+    // Set output message using logger
     const stream: StreamOptions = {
       write: message => this._logger.log(message.trim()),
     };
+    // Set skip condition
     const skip = () => false;
-    morgan(':method :url :status - :response-time ms', {
+
+    // Run morgan
+    const morganMiddleware = morgan(':method :url :status - :response-time ms', {
       stream,
       skip,
-    })(req, res, next);
+    });
+    morganMiddleware(req, res, next);
   }
 }
