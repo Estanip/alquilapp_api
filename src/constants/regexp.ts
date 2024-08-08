05eaf34da1c39a45950b2c1760c9d21646b46e6e
estanip <estani.pettigrew@gmail.com>
2024-08-08 15:00:00 -0300

    feat: add reservation date reg exp

--- src/constants/regexp.ts
+++ src/constants/regexp.ts
@@ -1,5 +1,6 @@
-export const emailRegExp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-export const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-export const userIdRegExp = /^[a-f\d]{24}$/i;
-export const dateRegExp = /^\d\d\d\d-\d\d-\d\d$/i;
-export const timeRegExp = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+export const EmailRegExp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+export const PasswordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+export const UserIdRegExp = /^[a-f\d]{24}$/i;
+export const DateRegExp = /^\d\d\d\d-\d\d-\d\d$/i;
+export const TimeRegExp = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+export const ReservationDateRegExp = /[0-9]+-(0?[1-9]|[1][0-2])-[0-9]+/i;
