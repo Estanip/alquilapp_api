1e46c7db0ecd909b681800e0cefbf6153a140f58
estanip <estani.pettigrew@gmail.com>
2024-08-27 09:50:52 -0300

    refactor: types

--- src/shared/database/repository/abstract.repository.ts
+++ src/shared/database/repository/abstract.repository.ts
@@ -2,17 +2,10 @@ import { Filter } from 'src/shared/database/interfaces';
 
 export abstract class AbstractDatabaseRepository<T, F = Filter<T>> {
   abstract create(item: T): Promise<T>;
-
   abstract deleteById(_id: string): Promise<void>;
-
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  abstract deleteOne(field: any, value: any): Promise<void>;
-
+  abstract deleteOne(field: string, value: string): Promise<void>;
   abstract findAll(filterQuery?: F): Promise<T[]>;
-
   abstract findById(id: string): Promise<T | null>;
-
   abstract findOne(filterQuery: F): Promise<T>;
-
   abstract findByIdAndUpdate(id: string, item: Partial<T>): Promise<T>;
 }
